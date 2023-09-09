022e5d6dd40246685b190dbb4142de74dc89277d
Tim Anhalt <tim@anhalt.me>
2023-09-09 20:34:54 +0200

    Started with file structure

---
+++ src/pages/Home/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+function HomePage() {
+    return <React.Fragment>
+        <Navbar/>
+        <h1>Home</h1>
+    </React.Fragment>;
+}
+
+export default HomePage;
